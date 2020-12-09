a6f48e07ea2c64cd7d07a912f83c1a961ccc2cec
CBURBANO <carotecno5@gmail.com>
2020-12-09 09:56:07 -0500

    Parcial

---
+++ src/java/BL/clsLogin.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BL;
+
+/**
+ *
+ * @author CAROLINA
+ */
+public class clsLogin {
+    
+    public boolean validarLogin(Models.clsLogin obclsLogin){
+    try {
+    
+        return obclsLogin.getStEmail().equals("cburbano@gmail.com")&&
+                obclsLogin.getStPassword().equals("123456");
+        
+    }catch(Exception ex){
+        throw ex;
+    
+    }
+
+}
+    
+}
